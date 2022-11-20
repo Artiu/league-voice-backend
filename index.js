d15c32144bec76f4329dd70285a01fced0d93c8a
Artiu <71273256+Artiu@users.noreply.github.com>
2022-11-20 04:00:51 +0100

    Add auth and rooms

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ import { Server } from "socket.io";
 const port = process.env.PORT || 3001;
 const io = new Server({ cors: { origin: "*" } });
 
+io.use((socket, next) => {
+    if (!socket.handshake.auth.token) {
+        return next(new Error("Not authorized"));
+    }
+    next();
+});
+
 io.on("connection", (socket) => {
     console.log(socket.id + " connected");
-    socket.broadcast.emit("userJoined", socket.id);
+    const authData = { id: socket.id, token: socket.handshake.auth.token };
+    socket.on("matchStart", (chatId) => {
+        console.log(socket.rooms);
+        socket.join(chatId);
+        socket.broadcast.to(chatId).emit("userJoined", authData);
+    });
     socket.on("offer", (offer, to) => {
-        socket.to(to).emit("offer", offer, socket.id);
+        socket.to(to).emit("offer", offer, authData);
     });
     socket.on("answer", (answer, to) => {
         socket.to(to).emit("answer", answer, socket.id);
     });
     socket.on("iceCandidate", (iceCandidate, to) => {
-        socket.to(to).emit("iceCandidate", iceCandidate, socket.id)
-    })
+        socket.to(to).emit("iceCandidate", iceCandidate, socket.id);
+    });
 });
 
 io.listen(port);
