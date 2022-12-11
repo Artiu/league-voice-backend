49deb49f169410f630cc52b22ef3425ccec27cf2
Artiu <71273256+Artiu@users.noreply.github.com>
2022-12-11 22:37:37 +0100

    Fix updater

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ const httpServer = createServer(app);
 
 const releaseAppInfo = {
     url: "https://api.league-voice.site/releases/LeagueVoice.zip",
-    version: "0.1.0",
-    notes: "Fix champion images",
+    version: "0.2.0",
+    notes: "Fix updater",
     signature:
-        "dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVRS0ViL2tKVmwwYlpKc3F6Y1l4YnRTdTVOb2srV2p5b2cxRHhEVFlyYXhSdkh0Sy9FTDhsT0NLR2g1ODBHMUE0Zy9jeUE4eE00WlZLbWo5MFlKNk1HSHFSQzNkcTRnUXc0PQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNjcwNzc2MTQ5CWZpbGU6TGVhZ3VlIFZvaWNlXzAuMS4wX3g2NF9lbi1VUy5tc2kuemlwCmw2bXFDYnV4VzFYVmJSeFhJUytiaStKNFlZOGtPM3g3ckE2eHB4c0wxNUhaNUtuOVo0M1pJZnVod3JTSTlFYWZnakwxc2tPcFQxRk5QSFVVZVBRN0FBPT0K",
+        "dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVRS0ViL2tKVmwwYlZiYXVyZERWL2RSaDJjL3BoU3o2ZG1JMmxVZEdweTEyYTdxa2VkcUZEYzRPdC9sV0NkUXA2N0pVc1ZYY3BKdEplZmRxRFpqU2lmMUozdGhlR2xXbkFjPQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNjcwNzk0NDAyCWZpbGU6TGVhZ3VlIFZvaWNlXzAuMi4wX3g2NF9lbi1VUy5tc2kuemlwClN3UlR4VVBNOXhoWXB4Um9QU09YRkErdGk1dlk1aWlrSWJYaGZLUzIvSmVoWkJvWk1GSzROMXBPWW5EUllMbWIzU1VDRmtScTRpN2s2MG5sWTcrMUNBPT0K",
 };
 
 app.use("/releases", express.static("releases"));
