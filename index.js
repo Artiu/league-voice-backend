0780a703f363779db5340b05bd781e8d1cec1cdd
Artiu <71273256+Artiu@users.noreply.github.com>
2022-12-12 22:29:44 +0100

    Security update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { Server } from "socket.io";
 import { config } from "dotenv";
 import { getCurrentMatchBySummonerId, getSummonerByName } from "./riotApi.js";
 import { RateLimiterMemory } from "rate-limiter-flexible";
+import helmet from "helmet";
 
 config();
 const port = process.env.PORT || 3001;
@@ -28,6 +29,9 @@ const releaseAppInfo = {
     signature:
         "dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVRS0ViL2tKVmwwYlZiYXVyZERWL2RSaDJjL3BoU3o2ZG1JMmxVZEdweTEyYTdxa2VkcUZEYzRPdC9sV0NkUXA2N0pVc1ZYY3BKdEplZmRxRFpqU2lmMUozdGhlR2xXbkFjPQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNjcwNzk0NDAyCWZpbGU6TGVhZ3VlIFZvaWNlXzAuMi4wX3g2NF9lbi1VUy5tc2kuemlwClN3UlR4VVBNOXhoWXB4Um9QU09YRkErdGk1dlk1aWlrSWJYaGZLUzIvSmVoWkJvWk1GSzROMXBPWW5EUllMbWIzU1VDRmtScTRpN2s2MG5sWTcrMUNBPT0K",
 };
+
+app.use(helmet());
+app.disable("x-powered-by");
 
 app.use("/releases", express.static("releases"));
 
