09d482cbd1b903e9d9cb1b5a37a6d37f2302c226
Artiu <oborskiartur123@gmail.com>
2023-11-24 20:19:15 +0100

    Update app

--- index.js
+++ index.js
@@ -13,21 +13,21 @@ const app = express();
 const httpServer = createServer(app);
 
 const authRateLimiter = new RateLimiterMemory({
-    points: 5,
-    duration: 60,
+	points: 5,
+	duration: 60,
 });
 
 const matchStartRateLimiter = new RateLimiterMemory({
-    points: 1,
-    duration: 1,
+	points: 1,
+	duration: 1,
 });
 
 const releaseAppInfo = {
-    url: "https://api.league-voice.site/releases/LeagueVoice.zip",
-    version: "0.5.2",
-    notes: "Fixes",
-    signature:
-        "dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVRS0ViL2tKVmwwYmJyY1RXbWhNY0JsTklWNmRxVUVsbG96Y09LQVhBc3VQR2RFRnlWM0I0NCtBMWJnNllVa2NEZlRsWDVjd0Q2NWNTRzhkV3pFTlVWbDd1emtXREN1bWdjPQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNjcyMDEwNjAxCWZpbGU6TGVhZ3VlIFZvaWNlXzAuNS4yX3g2NF9lbi1VUy5tc2kuemlwCjE0TGRHbmN4dTlCWE9OT0R3OExKQnZrSy9Bd0JvUG0yWW9wRlJIV3RhaVFuaW9Jd3pzTytEeXZ0d2FjM3VEeHZXZ3RvRjhGaGVHSWp2S2VIakVZMEJ3PT0K",
+	url: "https://league-voice-api.artiu.dev/releases/LeagueVoice.zip",
+	version: "0.5.3",
+	notes: "URL Change",
+	signature:
+		"dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVRS0ViL2tKVmwwYlNhRy8xOExxQUhPN1BoMTY4elBLaExIVWVRbTVMc1dvOVJQR3B5dFF6R09KcVdQK08rMW5MRjRqVCtaYXpYRGNoV1hrRVhBUGVhOXByQXZXbDJQTndZPQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzAwODUyMjA2CWZpbGU6TGVhZ3VlIFZvaWNlXzAuNS4zX3g2NF9lbi1VUy5tc2kuemlwCmFJWXMrUDlkaHNGRDNRMStEblI0Nko2akxxOWdRa1FRUjNtQWVKNms3UW9IeitFTE9WMTNzczY1ajdudGRhTWJmQUVTUGxqUmpjOU9sQ0hHbVNNa0NnPT0K",
 };
 
 app.use(helmet());
@@ -36,98 +36,98 @@ app.disable("x-powered-by");
 app.use("/releases", express.static("releases"));
 
 app.get("/releases/:version", (req, res) => {
-    if (req.params.version === releaseAppInfo.version) {
-        return res.sendStatus(204);
-    }
-    res.status(200).json(releaseAppInfo);
+	if (req.params.version === releaseAppInfo.version) {
+		return res.sendStatus(204);
+	}
+	res.status(200).json(releaseAppInfo);
 });
 
 const io = new Server(httpServer, {
-    cors: {
-        origin: ["http://localhost:1420", "https://tauri.localhost", "https://league-voice.site"],
-    },
+	cors: {
+		origin: ["http://localhost:1420", "https://tauri.localhost", "https://league-voice.site"],
+	},
 });
 
 io.use(async (socket, next) => {
-    const ip = socket.handshake.headers["x-real-ip"];
-    try {
-        await authRateLimiter.consume(ip);
-    } catch {
-        console.log(`Ip address: ${ip} tried to log in more than 5 times in minute`);
-        return next(new Error("rate-limit"));
-    }
-    if (!socket.handshake.auth.summonerName) {
-        return next(new Error("unauthorized"));
-    }
-    const summonerData = await getSummonerByName(socket.handshake.auth.summonerName);
-    if (!summonerData) {
-        return next(new Error("unauthorized"));
-    }
-    socket.summoner = summonerData;
-    next();
+	const ip = socket.handshake.headers["x-real-ip"];
+	try {
+		await authRateLimiter.consume(ip);
+	} catch {
+		console.log(`Ip address: ${ip} tried to log in more than 5 times in minute`);
+		return next(new Error("rate-limit"));
+	}
+	if (!socket.handshake.auth.summonerName) {
+		return next(new Error("unauthorized"));
+	}
+	const summonerData = await getSummonerByName(socket.handshake.auth.summonerName);
+	if (!summonerData) {
+		return next(new Error("unauthorized"));
+	}
+	socket.summoner = summonerData;
+	next();
 });
 
 io.on("connection", (socket) => {
-    console.log(
-        `${socket.id}(${socket.summoner.name}) connected\nCurrent user count: ${io.engine.clientsCount}`
-    );
-    const authData = { id: socket.id, summonerName: socket.summoner.name };
-    const leavePreviousCall = () => {
-        socket.rooms.forEach((value) => {
-            if (value !== socket.id) {
-                socket.to(value).emit("userLeft", authData);
-                socket.leave(value);
-                console.log(`${socket.id}(${socket.summoner.name}) left ${value} room`);
-            }
-        });
-    };
-    const onMatchStarted = async () => {
-        try {
-            await matchStartRateLimiter.consume(socket.id);
-        } catch {
-            return;
-        }
-        const matchData = await getCurrentMatchBySummonerId(socket.summoner.id);
-        if (!matchData) return;
-        const summonerTeam = matchData.participants.find(
-            (participant) => participant.summonerName === socket.summoner.name
-        ).teamId;
-        const teammates = matchData.participants.filter(
-            (participant) => participant.teamId === summonerTeam
-        );
-        const roomName = matchData.gameId + "-" + summonerTeam;
-        if (
-            (await io.in(roomName).fetchSockets()).find(
-                (socketFromRoom) => socketFromRoom.summoner.name === authData.summonerName
-            )
-        ) {
-            socket.emit("matchStarted", { error: "account-currently-in-room" });
-            return;
-        }
-        socket.emit("matchStarted", teammates);
-        socket.join(roomName);
-        console.log(`${socket.summoner.name} joined ${roomName} room`);
-        socket.broadcast.to(roomName).emit("userJoined", authData);
-    };
+	console.log(
+		`${socket.id}(${socket.summoner.name}) connected\nCurrent user count: ${io.engine.clientsCount}`
+	);
+	const authData = { id: socket.id, summonerName: socket.summoner.name };
+	const leavePreviousCall = () => {
+		socket.rooms.forEach((value) => {
+			if (value !== socket.id) {
+				socket.to(value).emit("userLeft", authData);
+				socket.leave(value);
+				console.log(`${socket.id}(${socket.summoner.name}) left ${value} room`);
+			}
+		});
+	};
+	const onMatchStarted = async () => {
+		try {
+			await matchStartRateLimiter.consume(socket.id);
+		} catch {
+			return;
+		}
+		const matchData = await getCurrentMatchBySummonerId(socket.summoner.id);
+		if (!matchData) return;
+		const summonerTeam = matchData.participants.find(
+			(participant) => participant.summonerName === socket.summoner.name
+		).teamId;
+		const teammates = matchData.participants.filter(
+			(participant) => participant.teamId === summonerTeam
+		);
+		const roomName = matchData.gameId + "-" + summonerTeam;
+		if (
+			(await io.in(roomName).fetchSockets()).find(
+				(socketFromRoom) => socketFromRoom.summoner.name === authData.summonerName
+			)
+		) {
+			socket.emit("matchStarted", { error: "account-currently-in-room" });
+			return;
+		}
+		socket.emit("matchStarted", teammates);
+		socket.join(roomName);
+		console.log(`${socket.summoner.name} joined ${roomName} room`);
+		socket.broadcast.to(roomName).emit("userJoined", authData);
+	};
 
-    const onSignaling = (data, to) => {
-        let isInTheSameMatch = false;
-        socket.rooms.forEach((roomId) => {
-            if (roomId === socket.id) return;
-            isInTheSameMatch = io.sockets.sockets.get(to)?.rooms.has(roomId);
-        });
-        if (!isInTheSameMatch) return;
-        socket.to(to).emit("signaling", data, authData);
-    };
+	const onSignaling = (data, to) => {
+		let isInTheSameMatch = false;
+		socket.rooms.forEach((roomId) => {
+			if (roomId === socket.id) return;
+			isInTheSameMatch = io.sockets.sockets.get(to)?.rooms.has(roomId);
+		});
+		if (!isInTheSameMatch) return;
+		socket.to(to).emit("signaling", data, authData);
+	};
 
-    socket.on("matchStart", onMatchStarted);
-    socket.on("signaling", onSignaling);
-    socket.on("leaveCall", () => {
-        leavePreviousCall();
-    });
-    socket.on("disconnecting", () => {
-        leavePreviousCall();
-    });
+	socket.on("matchStart", onMatchStarted);
+	socket.on("signaling", onSignaling);
+	socket.on("leaveCall", () => {
+		leavePreviousCall();
+	});
+	socket.on("disconnecting", () => {
+		leavePreviousCall();
+	});
 });
 
 httpServer.listen(port);
